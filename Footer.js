641dddcca21411e73aad3bd66f726406b7ffc862
IfeanyiChielo <50734342+IfeanyiChielo@users.noreply.github.com>
2021-07-17 16:58:57 +0100

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,8 @@
+import React from "react"
+function Footer() {
+return (
+        <Footer> This is my wonderful footer </Footer>
+)
+}
+
+export default Footer
---
+++ Header.js
@@ -0,0 +1,8 @@
+import React from "react"
+function Header() {
+return (
+        <header> This is my header </header>
+)
+}
+
+export default Header
